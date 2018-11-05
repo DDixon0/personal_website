cbe8946f713cce4cb0d1835ce64003331c7b12c8
Mamba0 <dixon.denzell@gmail.com>
2018-11-04 19:41:15 -0500

    errors

--- styles/scripts/myscript.js
+++ styles/scripts/myscript.js
@@ -1,1 +1,18 @@
-alert("My personal Website is currently under construction");
+alert("My personal website is currently under construction");
+
+
+
+
+//The Welcome sign is going to animate, then the sign underneath will load
+const titlelist = document.getElementsByTagName("h1");
+const title = titlelist[0];
+const blurbs = document.getElementsByTagName("h4");
+const blurb = blurbs[0];
+
+// title.addEventListener("animationend", flip); 
+
+
+function flip(){
+
+    blurb.style.opacity= "1";
+}
